69600bbfe7e8b37fb304d6db4f2f4fc5026dfdb0
Jared Henderson <jared@netrivet.com>
2021-06-11 09:41:56 -0400

    support un-symlinking with --unlink/-u

--- src/main.c
+++ src/main.c
@@ -9,33 +9,53 @@
 #include "shared.h"
 #include "string.h"
 
-typedef struct flags_t {
+typedef struct opts_t {
   bool print;
   bool cwd_only;
-} flags_t;
+  bool unlink;
+} opts_t;
 
-flags_t parse_argv(int argc, char** argv);
+opts_t parse_argv(int argc, char** argv);
 void symlink_package(pkg_t* pkg);
+void unsymlink_package(pkg_t* pkg);
 
 int main(int argc, char** argv) {
-  flags_t flags = parse_argv(argc, argv);
+  opts_t opts = parse_argv(argc, argv);
   pkgs_t* packages = load_packages(get_flroot());
   if (!packages)
     ABORT("Error loading packages from FLROOT");
 
-  if (flags.print) {
+  if (opts.print) {
     print_packages(packages);
     exit(EXIT_SUCCESS);
   }
 
   for (int i = 0; i < packages->count; i++) {
     pkg_t* pkg = packages->map[i];
-    if (!flags.cwd_only || str_is(pkg->abspath, cwd())) {
-      symlink_package(pkg);
+    if (!opts.cwd_only || str_is(pkg->abspath, cwd())) {
+      opts.unlink ? unsymlink_package(pkg) : symlink_package(pkg);
     }
   }
 
   exit(EXIT_SUCCESS);
+}
+
+void unsymlink_package(pkg_t* pkg) {
+  if (str_is(pkg->name, "@friends-library/native"))
+    return;
+
+  printf(C_GREY "Un-symlinking deps for package: %s%s\n", C_GREEN, pkg->name);
+  for (int i = 0; i < pkg->num_deps; i++) {
+    pkg_t* dep = pkg->deps[i];
+    char* dep_path = path_joinv(pkg->abspath, "node_modules", dep->name, NULL);
+    char* bak_path = str_join(dep_path, ".bak");
+
+    if (is_symlink(dep_path) && unlink(dep_path))
+      ABORT("unable to remove existing symlink at `%s`\n", dep_path);
+
+    if (is_dir(bak_path) && rename(bak_path, dep_path))
+      ABORT("unable to restore non-symlink dir at `%s`\n", bak_path);
+  }
 }
 
 void symlink_package(pkg_t* pkg) {
@@ -49,7 +69,7 @@ void symlink_package(pkg_t* pkg) {
 
     // step 1, rename any existing, non-symlink npm-install-ed dir
     if (is_dir(dep_path) && !is_symlink(dep_path))
-      if (rename(dep_path, str_join(dep_path, ".bak")) != 0)
+      if (rename(dep_path, str_join(dep_path, ".bak")))
         ABORT("non-symlink rename of `%s` failed", dep_path);
 
     // step 2, if there is already a symlink, just delete it
@@ -61,20 +81,22 @@ void symlink_package(pkg_t* pkg) {
       ABORT("unexpected dir remaining at `%s`\n", dep_path);
 
     // create the symlink
-    if (symlink(dep->abspath, dep_path) != 0)
+    if (symlink(dep->abspath, dep_path))
       ABORT("error creating symblink %s -> %s\n", dep->abspath, dep_path);
   }
 }
 
-flags_t parse_argv(int argc, char** argv) {
-  flags_t flags = {0};
+opts_t parse_argv(int argc, char** argv) {
+  opts_t opts = {0};
   for (int i = 1; i < argc; i++) {
     char* arg = argv[i];
     if (str_is(arg, "--print") || str_is(arg, "-p")) {
-      flags.print = true;
+      opts.print = true;
+    } else if (str_is(arg, "--unlink") || str_is(arg, "-u")) {
+      opts.unlink = true;
     } else if (str_is(arg, ".")) {
-      flags.cwd_only = true;
+      opts.cwd_only = true;
     }
   }
-  return flags;
+  return opts;
 }
